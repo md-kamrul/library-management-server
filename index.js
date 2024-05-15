ce9718b80268d841e5da83c37b46080411447ca6
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-05-15 23:37:19 +0600

    borrow book delete function bug fix

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/borrowBook/:id', async (req, res) => {
+    app.delete('/borrowedBook/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await addBookListCollection.deleteOne(query);
