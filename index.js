86b75937a59cb40cf54d580637792540c0a2a70d
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-05-15 23:29:39 +0600

    borrow book function added

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/borrowBook/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addBookListCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/addBook/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
